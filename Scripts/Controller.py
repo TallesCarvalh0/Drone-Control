45269048736b75447c5111d48ba2e3f3cca31c18
TallesCarvalh0 <127156900+TallesCarvalh0@users.noreply.github.com>
2025-01-29 21:16:27 -0300

    Update Controller.py

--- Scripts/Controller.py
+++ Scripts/Controller.py
@@ -1,21 +1,17 @@
-
-#e_pixel_x = pixel_x_camera - pixel_x_operador
-#v_x = Kp * erro_pixel_x
-
-
 import asyncio
-
 import threading
 import cv2
 from mavsdk import System
-from mavsdk.offboard import (OffboardError, VelocityNedYaw,PositionNedYaw)
+from mavsdk.offboard import (OffboardError, VelocityNedYaw, PositionNedYaw)
 from gazebo_opencv import Video
 import customtkinter as ctk
-from PIL import Image,ImageTk
+from PIL import Image, ImageTk
 import os
 
 class VideoStream(ctk.CTk):
-    
+    """
+    Classe para gerenciar o stream de vídeo e a interface gráfica.
+    """
     def __init__(self):
         super().__init__()
         screen_width = self.winfo_screenwidth()
@@ -26,89 +22,109 @@ class VideoStream(ctk.CTk):
         self.title('LLC - LineLynx Commander')
         self._set_appearance_mode('light')
         
-        self.start_video_loop()
-        self.mainloop() 
+        self.start_video_loop()  # Inicia o loop de vídeo
+        self.mainloop()
 
     def start_video_loop(self):
-        # Aguarda
+        """
+        Inicia a captura de vídeo e configura o canvas para exibir o vídeo.
+        """
         self.after(1000)
 
-        self.video = Video()
-        self.video.start_gst()
-        self.video_canvas = ctk.CTkCanvas(self, bg='black')
-        self.video_canvas.place(relx=0.05, rely=0.1, anchor='nw', relwidth=0.7, relheight=0.8)
+        self.video = Video()  # Inicializa o objeto de captura de vídeo
+        self.video.start_gst()  # Inicia o pipeline de vídeo
         self.video_canvas = ctk.CTkCanvas(self, bg='black')
         self.video_canvas.place(relx=0.05, rely=0.1, anchor='nw', relwidth=0.7, relheight=0.8)
         self.video_canvas.bind("<Button-1>", self.on_canvas_click)  # Adiciona um evento de clique
         self.update_video()
 
     def on_canvas_click(self, event):
-        """Manipula o clique no vídeo"""
+        """
+        Manipula o clique no vídeo.
+        """
+        click_y = event.y  # Obtém a posição Y do clique no canvas
+        self.save_image_on_click(event)  # Chama função para salvar a imagem ao clicar
 
-        click_y=event.y   
-        self.save_image_on_click(event)
-        
-            
-    def save_image_on_click(self,event):
-        """Salva o frame completo ao clicar no vídeo"""
+    def save_image_on_click(self, event):
+        """
+        Salva o frame completo do vídeo quando o usuário clica na tela.
+        """
         if self.video.frame_available():
-            frame = self.video.frame()
+            frame = self.video.frame()  # Captura o frame atual
             
-            # Obtem as dimensões do frame
+            # Obtém as dimensões do frame
             frame_height, frame_width, _ = frame.shape
-            click_x = (event.x / self.video_canvas.winfo_width())*640
-            # Coordenadas do centro do frame
-            center_x = frame_width / 2
-            center_y = frame_height / 2
-            x_initial = center_x - click_x
+            click_x = (event.x / self.video_canvas.winfo_width()) * 640  # Calcula a posição X do clique
+            center_x = frame_width / 2  # Posição X do centro do frame
+            center_y = frame_height / 2  # Posição Y do centro do frame
+            x_initial = center_x - click_x  # Calcula a diferença em X
+            
             try:
-                with open('/home/talles/UFU/TCC/MAVSDK-Python/scripts/output.txt','w') as file:
+                # Grava a diferença calculada em um arquivo de saída
+                with open('MAVSDK-Python/scripts/output.txt', 'w') as file:
                     file.write(str(x_initial))
-            except Exception as e: 
+            except Exception as e:
                 print(f"Erro ao escrever: {e}")
-            # Redimensiona o frame
+            
+            # Redimensiona o frame para caber no canvas
             frame = cv2.resize(frame, (self.video_canvas.winfo_width(), self.video_canvas.winfo_height()))
         
     def update_video(self):
-        """Atualiza o frame do vídeo no Canvas"""
+        """
+        Atualiza o frame do vídeo no canvas.
+        """
         if self.video.frame_available():
             frame = self.video.frame()
 
+            # Redimensiona o frame
             frame = cv2.resize(frame, (self.video_canvas.winfo_width(), self.video_canvas.winfo_height()))
-            img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            img = Image.fromarray(img)
-            img = ImageTk.PhotoImage(img)
-            self.video_canvas.create_image(0, 0, anchor='nw', image=img)
-            self.video_canvas.image = img
+            img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # Converte o frame para RGB
+            img = Image.fromarray(img)  # Converte o array NumPy em uma imagem PIL
+            img = ImageTk.PhotoImage(img)  # Converte a imagem PIL para um objeto utilizável no tkinter
+            self.video_canvas.create_image(0, 0, anchor='nw', image=img)  # Exibe a imagem no canvas
+            self.video_canvas.image = img  # Mantém uma referência para a imagem
             
-
         self.video_canvas.after(10, self.update_video)  # Atualiza a cada 10 milissegundos
             
 def run_video_stream():
+    """
+    Função para rodar o stream de vídeo em uma thread separada.
+    """
     VideoStream()
 
-async def pixel2meters(x_initial,drone):
+async def pixel2meters(x_initial, drone):
+    """
+    Converte a distância inicial em pixels para metros com base na altitude do drone.
+    """
     async for position in drone.telemetry.position():
-        altitude = round(position.relative_altitude_m)
+        altitude = round(position.relative_altitude_m)  # Obtém a altitude do drone
         print(altitude)
         await asyncio.sleep(1)
-        pixeldensity = 231.71*(altitude**(-0.813)) # Quantos pixels equivale a 1 metro
         
-        x_initial_m = float(x_initial)/pixeldensity                     
+        # Calcula a densidade de pixels por metro com base na altitude
+        pixeldensity = 231.71 * (altitude ** (-0.813))  # Fórmula de densidade de pixels
+        
+        # Converte a distância inicial de pixels para metros
+        x_initial_m = float(x_initial) / pixeldensity
         break
     return x_initial_m
 
-async def get_position(east_m,drone):
+async def get_position(east_m, drone):
+    """
+    Obtém a posição atual do drone na direção leste.
+    """
     async for positionnow in drone.telemetry.position_velocity_ned():
-        east_m = round(positionnow.position.east_m,4)
+        east_m = round(positionnow.position.east_m, 4)  # Obtém a posição leste do drone
         await asyncio.sleep(1)
         break
     return east_m
         
 async def main():
-
+    """
+    Função principal que inicializa o drone e o sistema de controle.
+    """
     drone = System()
-    await drone.connect(system_address="udp://:14540")
+    await drone.connect(system_address="udp://:14540")  # Conecta ao drone
 
     print("Waiting for drone to connect...")
     async for state in drone.core.connection_state():
@@ -123,82 +139,87 @@ async def main():
             break
 
     print("-- Arming")
-    await drone.action.arm()
+    await drone.action.arm()  # Arma o drone
 
     print("-- Setting initial setpoint")
-    await drone.offboard.set_position_ned(PositionNedYaw(0.0, 0.0, 0.0, 0.0))
+    await drone.offboard.set_position_ned(PositionNedYaw(0.0, 0.0, 0.0, 0.0))  # Define a posição inicial
 
     print("-- Starting offboard")
     
     try:
-        await drone.offboard.start()
+        await drone.offboard.start()  # Inicia o modo offboard
     except OffboardError as error:
-        print(f"Starting offboard mode failed with error code: \
-              {error._result.result}")
+        print(f"Starting offboard mode failed with error code: {error._result.result}")
         print("-- Disarming")
-        await drone.action.disarm()
+        await drone.action.disarm()  # Desarma o drone em caso de falha
         return
     
-    
-    #Captura do ponto 
+    # Captura do ponto de clique
     video_stream_thread = threading.Thread(target=run_video_stream)
     video_stream_thread.start()
 
-    #Leva para uma posição inicial
+    # Leva o drone a uma posição inicial
     print("-- Go up 2 m")
     await drone.offboard.set_position_ned(PositionNedYaw(0.0, 0.0, -2.0, 0.0))
     await asyncio.sleep(7)
 
     tamanho = 0
-    #Distancia do ponto clicado para o centro
+    # Espera a captura do ponto clicado
     while tamanho == 0:
-        with open('/home/talles/UFU/TCC/MAVSDK-Python/scripts/output.txt','r') as file:
+        with open('MAVSDK-Python/scripts/output.txt', 'r') as file:
             x_initial = file.read()
             tamanho = os.fstat(file.fileno()).st_size
             
             if tamanho != 0:
                 print("sai aqui")
                 break
-            
-    #Captura a altura do drone e converte a distancia em pixels para metros
-    print(f"o valor do inicio é{x_initial}")
-                                
-    x_initial_m= await pixel2meters(x_initial,drone)
     
+    # Converte a distância em pixels para metros
+    print(f"o valor do inicio é {x_initial}")
+    x_initial_m = await pixel2meters(x_initial, drone)
     print(f"o valor em metros é {x_initial_m}")
     
-    #Controlador proporcional
+    # Controlador proporcional para movimentação do drone
     x_meters = 0
-    kp = 0.1
+    kp = 0.1  # Coeficiente proporcional
     east_m = 0
     
+    # Controla o movimento do drone até a posição desejada
     while abs(x_meters - x_initial_m) > 0.01:
+        erro = x_initial_m - x_meters  # Calcula o erro
+        print(f"O erro em metros é: {erro}")
+        velocidade = kp * erro  # Calcula a velocidade proporcional
+        print(f"A velocidade é: {velocidade}")
         
-        erro = x_initial_m - x_meters
-        print(f"O erro em metros é : {erro}")
-        velocidade = kp * erro
-        print(f"A velocidade é: {velocidade}") 
-        east_initial = await get_position(east_m,drone)
-        print (f'o valor em east initial: {east_initial}')
-        await drone.offboard.set_velocity_ned(VelocityNedYaw(0.0, -1* (velocidade), 0.0, 0.0))
+        # Obtém a posição inicial do drone
+        east_initial = await get_position(east_m, drone)
+        print(f'o valor em east initial: {east_initial}')
+        
+        # Envia o comando de velocidade para o drone
+        await drone.offboard.set_velocity_ned(VelocityNedYaw(0.0, -1 * (velocidade), 0.0, 0.0))
         await asyncio.sleep(0.25)
 
-        east_now = await get_position(east_m,drone)
+        # Obtém a posição do drone após o movimento
+        east_now = await get_position(east_m, drone)
         print(f'o valor em east now: {east_now}')
-        east_moved = -east_now +east_initial
-        print (f'o valor em east moved: {east_moved}')
-        x_meters =  x_meters + east_moved
+        
+        # Calcula a distância percorrida
+        east_moved = -east_now + east_initial
+        print(f'o valor em east moved: {east_moved}')
+        
+        # Atualiza a distância total em metros
+        x_meters = x_meters + east_moved
         print(f'o valor de x_meters é {x_meters}')
         
-    with open('/home/talles/UFU/TCC/MAVSDK-Python/scripts/output.txt','w') as file:
+    # Limpa o arquivo de saída
+    with open('MAVSDK-Python/scripts/output.txt', 'w') as file:
         print("limpado")
-    #desligar offboard mode
+    
+    # Desliga o modo offboard
     try:
         await drone.offboard.stop()
     except OffboardError as error:
-        print(f"Stopping offboard mode failed with error code: \
-              {error._result.result}")
+        print(f"Stopping offboard mode failed with error code: {error._result.result}")
 
 if __name__ == "__main__":
-    
-    asyncio.run(main())
+    asyncio.run(main())  # Executa a função principal
